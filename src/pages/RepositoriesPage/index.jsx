b75e0a4b8f0793252b695d5ddbcf87d9900a2b30
NasserSalim1 <nasser.salim.beserra@gmail.com>
2024-12-18 10:23:19 -0300

    Index e Style do profile

--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Profile from './Profile';
+
 import { Container, SideBar, Main } from './styles';
 
 function RepositoriesPage() {
   return( 
     <Container>
-      <SideBar>SideBar</SideBar>
+      <SideBar><Profile /></SideBar>
       <Main>Main</Main>
     </Container>
   );

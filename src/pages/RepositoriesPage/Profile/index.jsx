1627858464048dce9ba33abfdb7ebdd94020775c
NasserSalim1 <nasser.salim.beserra@gmail.com>
2025-01-05 11:02:25 -0300

    Adicionando dados do usuário

--- src/pages/RepositoriesPage/Profile/index.jsx
+++ src/pages/RepositoriesPage/Profile/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
-import { Container, Header, Avatar, Login, Name } from './styles'
+import { MdGroup, MdLocationCity, MdWork } from 'react-icons/md';
+
+import { Container, Header, Avatar, Login, Name, Inner, Data } from './styles';
 
 function Profile() {
   return (
@@ -10,6 +12,14 @@ function Profile() {
         <Login>NasserSalim1</Login>
         <Name>NasserSalim1</Name>
       </Header>
+      <Inner>
+        <Data> 
+          <MdGroup size={20}/> 
+          30&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;10&nbsp;<i>seguindo</i>
+        </Data>
+        <Data> <MdWork size={20}/> Tereos</Data>
+        <Data> <MdLocationCity size={20}/> São José do Rio Preto</Data>
+      </Inner>
     </Container>
   )
 }

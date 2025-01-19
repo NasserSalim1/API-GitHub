5d439c74437a82d89fd9f4c6cc02a09927999b33
NasserSalim1 <nasser.salim.beserra@gmail.com>
2025-01-19 16:26:00 -0300

    Finalizando contador de linguagens

--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -6,6 +6,8 @@ import Repositories from './Repositories';
 
 import { Container, SideBar, Main } from './styles';
 
+import { getLangsFrom } from '../../services/api'
+
 function RepositoriesPage() {
   const user = {
     name: 'Nasser Salim',
@@ -43,31 +45,27 @@ function RepositoriesPage() {
       name: 'Repo 4', 
       description: 'Descrição', 
       html_url: 'https://github.com/NasserSalim1/API-GitHub', 
-      language: 'Ruby'
+      language: 'Python'
     },
 
     {
       name: 'Repo 5', 
       description: 'Descrição', 
       html_url: 'https://github.com/NasserSalim1/API-GitHub', 
-      language: 'Java'
+      language: 'Python'
     },
 
     {
       name: 'Repo 6', 
       description: 'Descrição', 
       html_url: 'https://github.com/NasserSalim1/API-GitHub', 
-      language: 'TypeScript'
+      language: 'Python'
     },
   ];
 
-// Calculo dos filters
-  const languages = [
-    { name: 'JavaScript', count: 3, color: '#f1c40f' },
-    { name: 'Sheell', count: 2, color: '#95a5a6' },
-    { name: 'PHP', count: 1, color: '#3498db' },
-    { name: 'Ruby', count: 2, color: '#E03131' },
-  ]
+  const languages = getLangsFrom(repositories);
+
+  // console.log(stats);
 
   return( 
     <Container>
@@ -76,7 +74,7 @@ function RepositoriesPage() {
         <Filter languages={languages} />
       </SideBar>
       <Main>
-        <Repositories />
+        <Repositories repositories={repositories} />
       </Main>
     </Container>
   );

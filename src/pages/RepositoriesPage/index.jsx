dc857c4f3828e693367513f14cf50f3169c3f45c
NasserSalim1 <nasser.salim.beserra@gmail.com>
2025-01-21 18:33:11 -0300

    Componentes repositories

--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -16,11 +16,12 @@ function RepositoriesPage() {
     followers: 3,
     following: 3,
     company: 'Tereos',
-    location: 'São José do Rio PReto',
+    location: 'São José do Rio Preto',
   };
   
   const repositories = [
     {
+      id: '1',
       name: 'Repo 1', 
       description: 'Descrição', 
       html_url: 'https://github.com/NasserSalim1/API-GitHub', 
@@ -28,6 +29,7 @@ function RepositoriesPage() {
     },
 
     {
+      id: '2',
       name: 'Repo 2', 
       description: 'Descrição', 
       html_url: 'https://github.com/NasserSalim1/API-GitHub', 
@@ -35,6 +37,7 @@ function RepositoriesPage() {
     },
 
     {
+      id: '3',
       name: 'Repo 3', 
       description: 'Descrição', 
       html_url: 'https://github.com/NasserSalim1/API-GitHub', 
@@ -42,6 +45,7 @@ function RepositoriesPage() {
     },
 
     {
+      id: '4',
       name: 'Repo 4', 
       description: 'Descrição', 
       html_url: 'https://github.com/NasserSalim1/API-GitHub', 
@@ -49,6 +53,7 @@ function RepositoriesPage() {
     },
 
     {
+      id: '5',
       name: 'Repo 5', 
       description: 'Descrição', 
       html_url: 'https://github.com/NasserSalim1/API-GitHub', 
@@ -56,6 +61,7 @@ function RepositoriesPage() {
     },
 
     {
+      id: '6',
       name: 'Repo 6', 
       description: 'Descrição', 
       html_url: 'https://github.com/NasserSalim1/API-GitHub', 
@@ -64,8 +70,6 @@ function RepositoriesPage() {
   ];
 
   const languages = getLangsFrom(repositories);
-
-  // console.log(stats);
 
   return( 
     <Container>
